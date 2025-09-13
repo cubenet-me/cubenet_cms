64cb076f0eb00a330217c820698d4c5bd42da406
Vova <vovamasamusenkoyt@gmail.com>
2025-09-13 11:20:36 +0000

    Обновление моделей: удалены старые и добавлен новый models.py

--- engine/api/public/db/models/user.py
+++
@@ -1,9 +0,0 @@
-# engine/api/public/db/models/user.py
-from sqlalchemy import Column, Integer, String
-from engine.api.public.db.connection import Base
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    role = Column(String)
